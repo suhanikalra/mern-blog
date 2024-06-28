452da39d837051a7124094854e625d74820eae55
suhanikalra <k.suhani@iitg.ac.in>
2024-06-29 00:15:50 +0530

    sign-Up page ui and functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Signin from './pages/Signin'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-export default function Main() {
+export default function App() {
   return (
-   <BrowserRouter>
-   <Header />
-   <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/about" element={<About />}/>
-    <Route path="/sign-in" element={<Signin />}/>
-    <Route path="/sign-up" element={<SignUp />}/>
-    <Route path="/dashboard" element={<Dashboard />}/>
-    <Route path="/projects" element={<Projects />}/>
-
-
-    </Routes></BrowserRouter>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
