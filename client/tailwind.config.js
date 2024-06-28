e51e8de81cb9620a7fe3673b1fe0f41db86f4ed8
suhanikalra <k.suhani@iitg.ac.in>
2024-06-28 18:11:36 +0530

    created header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    './index.html',
     './src/**/*.{js,jsx,ts,tsx,html}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx,html}'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
