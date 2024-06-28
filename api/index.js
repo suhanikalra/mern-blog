e2c45c43b2306a182b53c160cc92f056217f8679
suhanikalra <k.suhani@iitg.ac.in>
2024-06-28 20:18:38 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+// // Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+
 });
