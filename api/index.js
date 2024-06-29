00a0bcc82bb9dcbb6c99950eae6d9ddc0b9f329d
suhanikalra <k.suhani@iitg.ac.in>
2024-06-29 17:51:17 +0530

    profile page and more

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -14,8 +15,11 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
@@ -24,13 +28,12 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
-app.use((err,req,res,next) => {
-    const statusCode = res.statusCode||500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-             });
-
-            });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Projects from './pages/Projects';
 import SignUp from './pages/SignUp';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -17,8 +18,10 @@ export default function App() {
         <Route path='/about' element={<About />} />
         <Route path='/Signin' element={<SignIn />} />
         <Route path='/SignUp' element={<SignUp />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/Projects' element={<Projects />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
+        <Route path='/projects' element={<Projects />} />
       </Routes>
       <Footer />
     </BrowserRouter>
